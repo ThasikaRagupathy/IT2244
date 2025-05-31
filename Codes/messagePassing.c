404b97629b8d1f33a8f11a3a8bba0da7ca625daa
Thasika Ragupathy <thasikathasi01@gmail.com>
2025-05-31 14:52:44 +0530

    Add files via upload

---
+++ Codes/messagePassing.c
@@ -0,0 +1,80 @@
+01.
+//send
+
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#define MAX 10
+
+//structure for message queue
+struct mesg_buffer{
+    long mesg_type;
+    char mesg_text[100];
+}message;
+
+int main()
+{
+    key_t key;
+    int msgid;
+
+    //ftok to generate unique key
+    key = ftok("send", 89);
+
+    //msgget creates a message queue
+    //and returns a identifier
+
+    msgid = msgget(key, 0666 | IPC_CREAT);
+    message.mesg_type = 1;
+    printf("Write data: ");
+    fgets(message.mesg_text,MAX,stdin);
+
+
+    //msgsnd to send message
+    msgsnd (msgid, &message, sizeof(message), 0);
+
+    //display the message
+    printf("Data send is : %s \n", message.mesg_text);
+    return 0;
+
+}
+
+================================================================================================================
+02.
+//receive
+
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+
+//structure for message queue
+
+struct mesg_buffer{
+long mesg_type;
+char mesg_text[100];
+}message;
+        
+int main()
+{
+    key_t key;
+    int msgid;
+
+    //ftok to generate unique key
+    key = ftok("send", 89);
+
+    //msgget creates a message queue
+    //and returns a identifier
+
+    msgid = msgget(key, 0666 | IPC_CREAT);
+
+    //msgrcv to receive message
+    msgrcv(msgid, &message, sizeof(message), 1, 0);
+
+    //display the message
+    printf("Data recieved is: %s \n", message.mesg_text);
+
+    //to destroy the message queue
+    msgctl(msgid, IPC_RMID, NULL);
+
+    return 0;
+
+}
